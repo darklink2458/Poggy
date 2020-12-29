aaad8b233ea74b9b5468d1308f11743ee107010c
President Foxman <2013islandboy@gmail.com>
2020-12-29 02:35:49 -0800

    Quotes Revamp
    Removal of old .txt Quotes saving.
    Now all quotes are saved to a Json format which makes it eaiser for management and allows each server to have their own set of quotes.

--- cogs/testingCog.py
+++ cogs/testingCog.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import json
 from discord.ext import commands,tasks
 
 class TestingCog(commands.Cog):
@@ -10,19 +11,38 @@ class TestingCog(commands.Cog):
     async def on_ready(self):
         print('TestingCog Loaded!')
 
-    @commands.command(aliases=['randQuote'], help = 'Produces a random quote from the ones saved with the addQuote command')
-    async def randquote(self, ctx):
-        lines = open('quotes.txt').read().splitlines()
-        myline = random.choice(lines)
-        #lines.close()
-        await ctx.send(myline)
+
+    #@commands.command(aliases=['randQuote'], help = 'Produces a random quote from the ones saved with the addQuote command')
+    #async def randquote(self, ctx):
+    #    lines = open('quotes.txt').read().splitlines()
+    #    myline = random.choice(lines)
+    #    #lines.close()
+    #    await ctx.send(myline)
+
+
+    @commands.command()
+    async def ting(self, ctx):
+      await ctx.send(ctx.message.guild.name)
 
 
     @commands.command(aliases=['addquote'], help = 'Adds a quote to the quote list.')
     async def addQuote(self, ctx, *, quote):
-        f = open("quotes.txt", "a")
-        f.write(quote + '\n')
-        f.close()
+        #Opens the Json Quote Database
+        file = open('quotes.json', 'r')
+        ServerQuotes = json.load(file)
+        server = ctx.message.guild.name
+
+        if server not in ServerQuotes:
+          ServerQuotes[server] = {'quoteNum':0,'quotes':{}}
+
+        curQuoteNum = ServerQuotes[server]['quoteNum'] + 1
+        ServerQuotes[server]['quoteNum'] = curQuoteNum
+        ServerQuotes[server]['quotes'][curQuoteNum] = quote
+
+        #Writes the Updated Database to the Json File
+        file = open('quotes.json', 'w')
+        json.dump(ServerQuotes, file)
+        file.close()
         await ctx.send('Quote Saved!')
 
 def setup(client):
